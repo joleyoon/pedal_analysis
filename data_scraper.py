3d8973e42cbdd3ce424129f8848718d26e0c44b0
Joel Yoon <joelyoon@Mac.lan>
2025-11-19 22:38:54 -0800

    added modifications

--- data_scraper.py
+++ data_scraper.py
@@ -6,12 +6,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import json
+import time
 
 
 class GearSearchScraper:
     SEARCH_URL = "https://www.thegearpage.net/board/index.php?search/&type=post"
 
-    def __init__(self, gear_query: str, *, headless: bool = False, timeout: int = 10) -> None:
+    def __init__(self, gear_query: str, *, headless: bool = False, timeout: int = 5) -> None:
         self.gear_query = gear_query
         self.headless = headless
         self.timeout = timeout
@@ -36,19 +37,27 @@ class GearSearchScraper:
 
     def perform_search(self) -> list[str]:
         """Fill the search box with the gear query and click search."""
-
         self.open_search_page()
         wait = WebDriverWait(self.driver, self.timeout)
 
         try:
+            # why does this not work when headless?
             search_input = wait.until(
                 EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[4]/div/div[3]/div[2]/div[2]/form/div/div/dl[1]/dd/ul/li[1]/input'))
             )
         except TimeoutException as exc:
             raise RuntimeError("Search input not found.") from exc
-
         search_input.send_keys(self.gear_query)
+        # this is the button part and quite honestly i have no idea how this works, cuz it just sends something and it searches itself
+        # starting from there? i have no idea, i thought it was suppose to press the button but i guess not either way it works so ima
+        # move on, if it doesnt work in the future cuz the search button isn't being pressed, do this part
 
+        self.press_search_button(wait)
+        time.sleep(5)
+
+        return self.gather_hrefs()
+
+    def press_search_button(self, wait) -> None:
         try:
             search_button = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[4]/div/div[3]/div[2]/div[2]/form/div/dl/dd/div/div[2]/button')
         except NoSuchElementException as exc:
@@ -57,59 +66,42 @@ class GearSearchScraper:
         wait.until(EC.element_to_be_clickable(search_button))
         search_button.click()
 
-        return self.gather_hrefs(max_results=50)
-
         
-    def gather_hrefs(self, *, max_results: int | None = None) -> list[str]:
+    def gather_hrefs(self) -> list[str]:
         """Collect hrefs inside '.block-body' sections, optionally bounded by max_results."""
-
-        contentRow = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.CLASS_NAME, "block-container")))
+        contentRow = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "block-container")))
         all_links = contentRow.find_elements(By.TAG_NAME, "a")
-
         links = []
         for link in all_links:
             links.append(link.get_attribute("href"))
 
         return links
 
-    def close(self) -> None:
-        """Close down the driver when we're done."""
-
-        self.driver.quit()
-
 
 if __name__ == "__main__":
     # gear = input("Enter the gear you want to search for: ").strip()
-    # gear = "prs silver sky"
-    # if not gear:
-    #     raise SystemExit("Gear search query cannot be empty.")
+    gear = "prs silver sky"
+    if not gear:
+        raise SystemExit("Gear search query cannot be empty.")
 
-    # scraper = GearSearchScraper(gear_query=gear, headless=False)
-    # export_list = scraper.perform_search()
+    scraper = GearSearchScraper(gear_query=gear, headless=False)
+    export_list = scraper.perform_search()
 
-    # print(export_list)
+    with open("something.json", 'w', encoding="utf-8") as f:
+        json.dump(export_list, f)
+        print("done")
 
-    # with open("something.json", 'w', encoding="utf-8") as f:
-    #     json.dump(f, export_list)
-    #     print("done")
+    # driver = webdriver.Chrome()
+    # driver.get("https://www.thegearpage.net/board/index.php?search/17466115/&q=prs+silver+sky&o=relevance")
 
-    driver = webdriver.Chrome()
-    driver.get("https://www.thegearpage.net/board/index.php?search/17445187/&q=prs+silver+sky&t=post&o=relevance")
+    # contentRow = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.CLASS_NAME, "block-container")))
+    # all_links = contentRow.find_elements(By.TAG_NAME, "a")
 
-    contentRow = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.CLASS_NAME, "block-container")))
-    all_links = contentRow.find_elements(By.TAG_NAME, "a")
+    # links = []
+    # for link in all_links:
+    #     links.append(link.get_attribute("href"))
 
-    links = []
-    for link in all_links:
-        links.append(link.get_attribute("href"))
-
-    print(len(links))
+    # print(len(links))
 
     while True:
         pass
-
-
-
-
-
-
